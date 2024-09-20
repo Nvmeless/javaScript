0edf4eccfe6eb7ea492aef95bace5d0cdad5b2bc
Jin <alexandre.Quilan.delaistre@gmail.com>
2024-09-20 15:46:21 +0200

    first commit

---
+++ index.js
@@ -0,0 +1,45 @@
+import LifeGame from './lifeGame.js'
+
+
+
+// var variable = "Bonjour";
+// let autreVariable = "Salut";
+// autreVariable = 1;
+// autreVariable = 1.3;
+// autreVariable = true;
+// autreVariable = false;
+// autreVariable = null;
+
+// autreVariable = undefined
+
+// let tableau = ['toto', "tata", "tpntn"]
+
+
+// tableau.push("Titi")
+
+// function displayCustomMessage(message){
+//     console.log("%c" + message,"color:red;font-size:large;")
+//     return message;
+// }
+
+// let displayKawaiMessage = (message,big) => console.log("%c" + message,"color:pink;font-size:" + ( big ? "large;": "small;"))  ;
+
+
+// let objet ={
+//     prenom: "Alexandre",
+//     nom:"Delaistre",
+//     age:26,
+//     city:"Lyon",
+//     bg:true,
+// }
+
+
+// let retourMessage = displayKawaiMessage('Helloooooo', true)
+//  retourMessage = displayKawaiMessage('Goooooooodbyyyye', false)
+// console.log(objet)
+// console.log(tableau)
+
+let lifeGame = new LifeGame(3)
+
+
+lifeGame.titi()
---
+++ lifeGame.js
@@ -0,0 +1,44 @@
+class LifeGame {
+
+    toto = 5;
+
+    constructor(tata){
+        console.log(this.toto)
+        this.toto = tata;
+        console.log(this.toto)
+
+    }
+    
+
+    titi = () => {
+        this.toto = 10
+        console.log(this.toto);
+    }
+
+
+
+
+
+    //Une methode qui creer un tableau de x / y
+        
+
+
+    //une methode qui recupere l'index dans un tableau par rapport a des coordonnées x et y
+
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+export default LifeGame;
